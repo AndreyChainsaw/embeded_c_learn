6f82b0fad1d6f3c042cd13cad47474ddd714314f
AndreyChainsaw <andreygor0502@mail.ru>
2023-08-25 19:31:40 +0700

    Добавлена поддержка канала (только один) в tinyShell

--- lection14/tinyShell.c
+++ lection14/tinyShell.c
@@ -6,51 +6,147 @@
 
 #define MAX_ARGS 11
 #define LEN_COMMAND 50
+#define MAX_PIPES 10
 
+struct execCandidat {
+  char **tokenPhrase;       // Команда, разбитая на токены по " "
+  unsigned char *pipes;     // индексы нахождения символов "|" в tokenPhrase
+  unsigned char countPipes; // Количество pipe в команде
+};
 
-// Поиск утилит должен быть в PATH. Пока можно думать, что она в PATH по-умолчанию
-// ls -la | grep bin (как работать с пайплайном?)
 
 char** tokenize(char*);
+void executeInstructions(struct execCandidat *parsedCmd);
 
 int main(int argc, char argv[]){
-	char command[LEN_COMMAND] = {0};
 
-	while (1) {
-		size_t lenFirstCommand = 0;
-		printf("# ");
-		fgets(command, LEN_COMMAND, stdin);
-		
-		command[strcspn(command, "\n")] = 0;
+  char rawCmd[LEN_COMMAND] = {0};
+  unsigned char pipes[MAX_PIPES];
+  struct execCandidat parsedCmd;
 
-		char **tokenPhrase = tokenize(command);
-		
-		pid_t childPID = fork();
+  while (1) {
+    size_t lenFirstCommand = 0;
+    printf("# ");
+    fgets(rawCmd, LEN_COMMAND, stdin);
 
-		if (0 == childPID) {
-			char fullPath[LEN_COMMAND + 6] = "/bin/"; // TODO: И что с размером делать?? Ерунда же полная...
+    pipes[0] = 0;
+    
+    // Заменяет символ переноса строки на NULL
+    rawCmd[strcspn(rawCmd, "\n")] = 0;
 
-			strcat(fullPath, *tokenPhrase);
-			execv(fullPath, tokenPhrase);
+    parsedCmd.tokenPhrase = tokenize(rawCmd);
+    parsedCmd.pipes = pipes;
+    parsedCmd.countPipes = 0;
+    
 
-		}
-		else {
-			int status = 0;
-			wait(&status);
-		}
-	}
-	return 0;
+    // Ищем символ pipe
+    for (int i = 0; parsedCmd.tokenPhrase[i] != NULL; i++) {
+      if (0 == strcmp("|", parsedCmd.tokenPhrase[i])) {
+        parsedCmd.countPipes++;
+        parsedCmd.pipes[parsedCmd.countPipes - 1] = i;
+      }
+    }
+
+    executeInstructions(&parsedCmd);
+    free(parsedCmd.tokenPhrase);
+    
+  }
+  return 0;
 
 }
 
+void executeInstructions(struct execCandidat *parsedCmd) {
+  pid_t childPID[parsedCmd->countPipes + 1];
+
+  // Пока считаем, что может быть только один канал
+  if (parsedCmd->countPipes > 0) {
+    pid_t childPID[2];
+    int pipefd[2];
+    pipe(pipefd);
+
+    childPID[0] = fork();
+
+    
+    if (childPID[0] == 0) {
+      close(pipefd[0]);
+      close(STDOUT_FILENO);
+
+      // Перенаправляю stdout в канал
+      dup(pipefd[1]);
+
+      char fullPath[LEN_COMMAND + 6] = "/bin/";
+      char *tokenPhrase[parsedCmd->pipes[0] + 1];
+
+      for (int i = 0; i < parsedCmd->pipes[0]; i++) {
+        tokenPhrase[i] = parsedCmd->tokenPhrase[i];
+      }
+      //Сделаем его NULL терминированным
+      tokenPhrase[parsedCmd->pipes[0]] = NULL;
+
+      strcat(fullPath, tokenPhrase[0]);
+      execv(fullPath, tokenPhrase);
+    }
+    
+    wait(NULL);
+
+    childPID[1] = fork();
+
+    if (childPID[1] == 0) {
+      close(STDIN_FILENO);
+      close(pipefd[1]);
+      dup(pipefd[0]);
+      close(pipefd[0]);
+
+
+      char fullPath[LEN_COMMAND + 6] = "/bin/";
+      char startCommand = parsedCmd->pipes[0] + 1;
+      
+      // Ищем конец массива
+      unsigned char endPhrase = startCommand;
+
+      while (parsedCmd->tokenPhrase[endPhrase] != NULL) {
+        endPhrase++;
+      }
+
+      // TODO: функцию по нахождению конца токенов
+      char *tokenPhrase[endPhrase];
+      
+      for (int i = 0; i < endPhrase; i++) {
+        tokenPhrase[i] = parsedCmd->tokenPhrase[i + startCommand];
+      }
+      //Сделаем его NULL терминированным
+      tokenPhrase[endPhrase] = NULL;
+
+      strcat(fullPath, tokenPhrase[0]);
+      execv(fullPath, tokenPhrase);
+
+    }
+    close(pipefd[1]);
+    close(pipefd[0]);
+    wait(NULL);
+  
+  }
+  else {
+    if (fork() == 0){
+      char fullPath[LEN_COMMAND + 6] = "/bin/";
+      strcat(fullPath, parsedCmd->tokenPhrase[0]);
+      execv(fullPath, parsedCmd->tokenPhrase);
+    }
+
+    wait(NULL);
+
+    
+  }
+}
+
 char** tokenize(char *phrase){
-	char *token = strtok(phrase, " ");
-	char **tokenPhrase = (char**) malloc(MAX_ARGS * sizeof(char*));
+  char *token = strtok(phrase, " ");
+  char **tokenPhrase = (char**) calloc(MAX_ARGS, sizeof(char*));
 
-	for (int i = 0; token != NULL && i < MAX_ARGS - 1; i++) {
-		tokenPhrase[i] = token;
-		token = strtok(NULL, " ");
-	}
+  for (int i = 0; token != NULL && i < MAX_ARGS - 1; i++) {
+    tokenPhrase[i] = token;
+    token = strtok(NULL, " ");
+  }
 
-	return tokenPhrase;
+  return tokenPhrase;
 }
